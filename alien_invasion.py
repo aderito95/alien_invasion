5adac027b5303a2eeb1a94cd454979902e1544f9
aderito <andreaderito95@gmail.com>
2024-02-14 17:18:48 +0100

    Refactor set_screen_resolution

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,18 +10,8 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
 
-        # Ottieni la risoluzione dello schermo principale
-        screen_modes = pygame.display.list_modes(0)
-        if screen_modes:
-            self.screen_width, self.screen_height = screen_modes[0]
-        else:
-            # Se non è disponibile alcuna informazione sulla risoluzione dello schermo, usa valori predefiniti
-            self.screen_width, self.screen_height = 800, 600
+        self.set_screen_resolution()
 
-        # Imposta la dimensione della finestra di gioco in base alla dimensione dello schermo
-        self.screen = pygame.display.set_mode(
-            (self.screen_width, self.screen_height - 60)
-        )
         pygame.display.set_caption("Alien invasion")
         # Imposta il colore di sfondo
         self.bg_color = (230, 230, 230)
@@ -41,6 +31,20 @@ class AlienInvasion:
             pygame.display.flip()
             # Pygame will do its best to make the loop run exactly 60 times per second
             self.clock.tick(60)
+
+    def set_screen_resolution(self):
+        """Ottieni la risoluzione dello schermo principale"""
+        screen_modes = pygame.display.list_modes(0)
+        if screen_modes:
+            self.screen_width, self.screen_height = screen_modes[0]
+        else:
+            # Se non è disponibile alcuna informazione sulla risoluzione dello schermo, usa valori predefiniti
+            self.screen_width, self.screen_height = 800, 600
+
+        # Imposta la dimensione della finestra di gioco in base alla dimensione dello schermo
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height - 60)
+        )
 
 
 # This code block will only run if this file is called directly
