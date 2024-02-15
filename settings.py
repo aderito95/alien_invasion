eff2abb5de7e675e04d3e293d13fdc406c50b797
aderito <andreaderito95@gmail.com>
2024-02-16 00:49:47 +0100

    fullscreen and add quit on escape press

--- settings.py
+++ settings.py
@@ -8,17 +8,4 @@ class Settings:
         """Initialize the game's settings"""
         # Screen settings
         self.bg_color = (230, 230, 230)
-        self.screen = Settings.get_screen_resolution()
-
-    @staticmethod
-    def get_screen_resolution():
-        """Ottieni la risoluzione dello schermo principale"""
-        screen_modes = pygame.display.list_modes(0)
-        if screen_modes:
-            screen_width, screen_height = screen_modes[0]
-        else:
-            # Se non è disponibile alcuna informazione sulla risoluzione dello schermo, usa valori predefiniti
-            screen_width, screen_height = 800, 600
-
-        # Imposta la dimensione della finestra di gioco in base alla dimensione dello schermo
-        return pygame.display.set_mode((screen_width, screen_height - 60))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
