8feaa112d8d02ab75e9605af8cc9cdddc0a00d2d
aderito <andreaderito95@gmail.com>
2024-02-23 17:53:34 +0100

    dropping the fleet and changing direction

--- settings.py
+++ settings.py
@@ -10,3 +10,5 @@ class Settings:
         self.bg_color = (230, 230, 230)
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.bullets_allowed = 3
+        # fleet direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
